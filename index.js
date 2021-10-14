67473b253d16cb82bf977c4600f5641566f2761d
Dayeane <dayeane85@gmail.com>
2021-10-13 23:49:12 -0700

    scope lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+
+var customerName = "bob"
+var bestCustomer
+const leastFavoriteCustomer = ""
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+// how can i make this pass if bob is modife already with uppercase
+function setBestCustomer() {
+    bestCustomer = `not ${customerName}`
+}
+
+function overwriteBestCustomer(text1) {
+    bestCustomer = text1
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "thank you"
+}
